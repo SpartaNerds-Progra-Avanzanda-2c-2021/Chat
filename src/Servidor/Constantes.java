15985f06fd4b372db254873414427656001c5a49
Gianluca <gianespin@hotmail.com>
2021-11-06 07:21:24 -0300

    se borra todo

--- src/Servidor/Constantes.java
+++
@@ -1,6 +0,0 @@
-package servidor;
-
-public class Constantes {
-	public static int clientesSoportados = 20;
-	public static int puerto = 10000;
-}
--- src/servidor_manejadores/ManejadorDelServidor.java
+++
@@ -1,12 +0,0 @@
-package servidor_manejadores;import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
-
-import app.Lobby;
-import servidor.ServerClient;
-import utils.Peticion;
-
-public abstract class ManejadorDelServidor<T> {
-	public abstract void manejar(Peticion<T> peticion, ArrayList<ServerClient> clientes, ServerClient solicitante, Lobby lobby)
-			throws UnknownHostException, Exception;
-}
